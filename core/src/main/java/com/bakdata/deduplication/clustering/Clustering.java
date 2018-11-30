7a3250938f567e1fdebe6943276d44fb23517f27
Arvid Heise <arvid.heise@bakdata.com>
2018-11-30 15:25:41 +0100

    Made cluster id comparable

--- core/src/main/java/com/bakdata/deduplication/clustering/Clustering.java
+++ core/src/main/java/com/bakdata/deduplication/clustering/Clustering.java
@@ -1,9 +1,13 @@
 package com.bakdata.deduplication.clustering;
 
 import com.bakdata.deduplication.classifier.ClassifiedCandidate;
+import lombok.NonNull;
 
 import java.util.List;
+import java.util.function.Function;
 
-public interface Clustering<CID, T> {
+public interface Clustering<CID extends Comparable<CID>, T> {
     List<Cluster<CID, T>> cluster(List<ClassifiedCandidate<T>> classified);
+
+    Function<Iterable<T>, CID> getClusterIdGenerator();
 }
